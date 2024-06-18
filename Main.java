7b9c237d5348910612d7b01a22cc85f895a3dd5f
rk <grenkiat@gmail.com>
2024-06-18 12:41:10 +0800

    create log file when running

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 public class Main {
-    Process process = new ProcessBuilder("java", "-version").start();
 
-    public Main() throws IOException {
-        InputStream inputStream = process.getInputStream();
-        System.out.println(inputStream);
+    public static void main(String[] args) throws IOException {
+
+        ProcessBuilder pb =
+                new ProcessBuilder("java", "-version");
+//        Map<String, String> env = pb.environment();
+//        env.put("VAR1", "myValue");
+//        env.remove("OTHERVAR");
+//        env.put("VAR2", env.get("VAR1") + "suffix");
+        pb.directory(new File(".\\myDir"));
+        File log = new File(".\\myDir\\log");
+        pb.redirectErrorStream(true);
+        pb.redirectOutput(ProcessBuilder.Redirect.appendTo(log));
+        Process p = pb.start();
+        assert pb.redirectInput() == ProcessBuilder.Redirect.PIPE;
+        assert pb.redirectOutput().file() == log;
+        assert p.getInputStream().read() == -1;
     }
 }
